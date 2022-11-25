7e24138453c6812630c6b1bd1b08a5adf3555c1c
Ali Zia <alizia068@gmail.com>
2022-11-26 01:49:31 +0500

    deploy testing

--- src/MainContent/BodyContent.js
+++ src/MainContent/BodyContent.js
@@ -36,8 +36,8 @@ const Home = () => {
       <Services/>
       <AboutUs/>
       <Portfolio/>
-      <WorkTogether/>
-  <ContactUs/>    
+      <ContactUs/>
+      <WorkTogether/>    
     </>
   );
 };
